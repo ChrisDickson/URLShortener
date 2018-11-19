30b22d797af15026490b36684480dc10c875e64f
Chris Dickson <chris.dickson1994@gmail.com>
2018-11-19 10:14:55 +0000

    Variable consistency
    Updated code so that the name used for the shortened URL is consistently 'short' rather than 'shortened'

--- app/shorten_url.py
+++ app/shorten_url.py
@@ -13,8 +13,6 @@ bp = Blueprint('shorten_url', __name__, url_prefix='/shorten_url')
 # Returns the shortened URL - Stores submitted URL in DB, generating unique key, returns unique key on end of domain
 def shorten_url():
     if request.method == 'POST':
-        shortened_url = ''
-
         req_json = request.get_json()
         url = req_json['url']
 
@@ -28,39 +26,45 @@ def shorten_url():
 
         elif re.match('(https|http)(\:\/\/www)(\.)(.+)(\.)(\w{2,3})(.+)', url) is False:
             error = 'Please enter an valid URL.'
-            
+
         elif db.execute(
             'SELECT id FROM urls WHERE url = ?', (url,)
         ).fetchone() is not None:
-            shortened_url = db.execute(
-                'SELECT short FROM urls WHERE url = ?', (url,)
-            )
-            return render_template('/short_url')
+            short_url = retrieve_short_url(url)
+            return render_template('/short_url', short_url, url)
 
         if error is None:
-            db.execute('INSERT INTO urls (url, shortened_url) VALUES ? ?',
-                (url, generate_shortened_url())
+            short_url = generate_shortened_url()
+            db.execute('INSERT INTO urls (url, shortened_url) VALUES ? ?', (url, short_url)
             )
             db.commit()
-            return render_template('/short_url', shortened_url, url)
+            return render_template('/short_url', short_url, url)
 
         flash(error)
 
-    return render_template('short_url')
+    return render_template('/short_url')
 
 
-@bp.route('/<short_url>')
-def return_original_url(short_url):
+@bp.route('/<short>')
+def return_original_url(short):
     db = get_db()
 
     original_url = db.execute(
-        'SELECT url FROM urls WHERE short = ?', (short_url,)
+        'SELECT url FROM urls WHERE short = ?', (short,)
     )
 
     return redirect(original_url)
 
 
-def generate_shortened_url():
-    shortened_url = ''.join(random.choice(string.ascii_uppercase, string.ascii_lowercase, string.digits) for _ in range(8))
+def retrieve_short_url(url):
+    db = get_db()
+    short = db.execute('SELECT short FROM urls where url = ?', ())
 
-    return shortened_url
+    return short
+
+
+def generate_shortened_url():
+    short_url = ''.join(random.choice(string.ascii_uppercase,
+                                          string.ascii_lowercase, string.digits) for _ in range(8))
+
+    return short_url
