de40ad32bc3878779345acc5a06c8ac665793195
Chris Dickson <chris.dickson1994@gmail.com>
2018-11-25 21:16:25 +0000

    Added/fixed DB interactions, updated short_url
    db.py now correctly works with MySQL, instead of SQLite. short_url now correctly generates a short url, displays it on 'short_url.html'

--- app/db.py
+++ app/db.py
@@ -9,8 +9,33 @@ def init_db(app):
     app.config['MYSQL_DATABASE_DB'] = 'urls'
     app.config['MYSQL_DATABASE_HOST'] = 'localhost'
     mysql.init_app(app)
-    print('test')
 
 
-def get_conn():
-    return 'e'
+def insert_urls(short, url):
+    con = mysql.connect()
+    cur = con.cursor()
+    cur.execute("INSERT INTO urls (url, short) VALUES(%s, %s)", url, short)
+    con.commit()
+
+
+def check_url_in_db(url):
+    url_flag = True
+    con = mysql.connect()
+    cur = con.cursor()
+    if cur.execute("SELECT * FROM urls.urls WHERE url = %s", url) is None:
+        url_flag = False
+    return url_flag
+
+
+def get_short(url):
+    con = mysql.connect()
+    cur = con.cursor()
+    short = cur.execute("SELECT short FROM urls.urls WHERE url = %s", url)
+    return short
+
+
+def get_original(short):
+    con = mysql.connect()
+    cur = con.cursor()
+    original = cur.execute("SELECT url FROM urls WHERE urls.short = %s", short)
+    return original
--- app/short_url.py
+++ app/short_url.py
@@ -1,10 +1,10 @@
 from flask import (
-    Blueprint, flash, render_template, request, redirect
+    Blueprint, render_template, request, redirect
 )
 import random
 import string
-import re
-from app.db import get_conn
+import validators
+from . import db
 
 bp = Blueprint('short_url', __name__)
 
@@ -12,60 +12,42 @@ bp = Blueprint('short_url', __name__)
 @bp.route('/short_url', methods=['POST', 'GET'])
 # Returns the shortened URL - Stores submitted URL in DB, generating unique key, returns unique key on end of domain
 def shorten_url():
-    print('1')
     if request.method == 'POST':
         req_json = request.get_json()
         url = req_json['url']
 
-        db = get_conn()
         error = None
 
         if not url:
             error = 'Please enter a URL to shorten.'
+            print(error)
 
-        elif url.startswith('https://') or url.startswith('http://') is False:
-            url = 'http://'+url
-
-        elif re.match('(https|http)(\:\/\/www)(\.)(.+)(\.)(\w{2,3})(.+)', url) is False:
+        elif validators.url(url) is False:
             error = 'Please enter an valid URL.'
+            print(error)
 
-        elif db.execute(
-            'SELECT id FROM urls WHERE url = ?', (url,)
-        ).fetchone() is not None:
-            short_url = retrieve_short_url(url)
-            return render_template('/short_url', short_url, url)
+        # True here means URL given is in the database already,
+        # and we can retrieve and return the shortened one that already exists
+        elif db.check_url_in_db(url) is True:
+            short_url = db.get_short(url)
+
+            return render_template('/short_url.html', short_url=short_url, url=url)
 
         if error is None:
             short_url = generate_shortened_url()
-            db.execute('INSERT INTO urls (url, short) VALUES (?, ?)', (url, short_url)
-            )
-            db.commit()
-            return render_template('/short_url.html', short_url, url)
+            db.insert_urls(short_url, url)
 
-        flash(error)
-
-    return render_template('/short_url.html')
+            return render_template('/short_url.html', short_url=short_url, url=url)
+        else:
+            return render_template('/short_url.html',error=error)
 
 
 @bp.route('/url/<short>', methods=['GET'])
 def return_original_url(short):
-    db = get_conn()
-
-    original_url = db.execute(
-        'SELECT url FROM urls WHERE short = ?', (short,)
-    )
-
+    original_url = db.get_original()
     return redirect(original_url)
-
-
-def retrieve_short_url(url):
-    db = get_db()
-    short = db.execute('SELECT short FROM urls where url = ?', ())
-
-    return short
 
 
 def generate_shortened_url():
     short_url = ''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase+string.digits) for _ in range(8))
-
     return short_url
